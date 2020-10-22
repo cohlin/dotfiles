33e7e913b6e5eea85059eca546c116d942043ef0
Christian Öhlin <cohlin@Christians-MacBook-Pro.local>
2020-10-22 09:58:49 +0200

    fixes in install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 # Remove existing dot files
 
-if [ -f /home/chroh1/.bash_profile ]; then
+if [ -f ~/.bash_profile ]; then
     echo "rm ~/.bash_profile"
-    rm /home/chroh1/.bash_profile
+    rm ~/.bash_profile
 fi
 
-if [ -f /home/chroh1/.bashrc ]; then
+if [ -f ~/.bashrc ]; then
     echo "rm ~/.bashrc"
-    rm /home/chroh1/.bashrc
+    rm ~/.bashrc
 fi
 
-if [ -f /home/chroh1/.gitconfig ]; then
+if [ -f ~/.gitconfig ]; then
     echo "rm ~/.gitconfig"
-    rm /home/chroh1/.gitconfig
+    rm ~/.gitconfig
 fi
 
-if [ -f /home/chroh1/.npmrc ]; then
+if [ -f ~/.npmrc ]; then
     echo "rm ~/.npmrc"
-    rm /home/chroh1/.npmrc
+    rm ~/.npmrc
 fi
 
-if [ -f /home/chroh1/.bash_aliases ]; then
+if [ -f ~/.bash_aliases ]; then
     echo "rm ~/.bash_aliases"
-    rm /home/chroh1/.bash_aliases
+    rm ~/.bash_aliases
 fi
 
 # Dotfiles
-ln -s $(pwd)/.bashrc /home/chroh1/.bashrc
-ln -s $(pwd)/.bash_profile /home/chroh1/.bash_profile
-ln -s $(pwd)/.bash_aliases /home/chroh1/.bash_aliases
-ln -s $(pwd)/.gitconfig /home/chroh1/.gitconfig
+ln -s $(pwd)/.bashrc ~/.bashrc
+ln -s $(pwd)/.bash_profile ~/.bash_profile
+ln -s $(pwd)/.bash_aliases ~/.bash_aliases
+ln -s $(pwd)/.gitconfig ~/.gitconfig
